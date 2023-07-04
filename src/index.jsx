0c2251ead059cd3f9d6fc3de17584d5e1cd373e8
Sanju Shah <sanjushahgupta@gmail.com>
2023-07-04 16:02:55 +0200

    feat: add react, react dom and parcel dependencies

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import "./index.scss";
+
+const MovieApplication = () => {
+    return (
+        <div className='movie'>
+            <div>Good morning</div>
+        </div>
+    );
+};
+
+const container = document.querySelector('#root');
+const root = createRoot(container);
+root.render(<MovieApplication />);
