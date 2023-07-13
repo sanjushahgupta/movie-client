a34988033af26426b6018ef3342fb3e563656f86
Sanju Shah <sanjushahgupta@gmail.com>
2023-07-13 12:05:32 +0200

    feat: delete fav movie from list

--- src/components/favouriteMovies/favouriteMovies.jsx
+++ src/components/favouriteMovies/favouriteMovies.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { Col } from "react-bootstrap"
+import { Col, Button, Row } from "react-bootstrap"
 import { MovieCard } from "../movie-card/movie-card";
+import { BaseUrl } from "../../constants/constant";
 
 export const FavoriteMovies = ({ user, movies }) => {
+    const token = localStorage.getItem("token");
     const [favMovies, setFavMovies] = useState([])
     useEffect(() => {
         if (user && user.favoriteMovies) {
@@ -15,18 +17,38 @@ export const FavoriteMovies = ({ user, movies }) => {
         return <div>Loading user data</div>;
     }
 
+    const removeFavHandler = (movieTitle, event) => {
+        event.preventDefault();
+        fetch(BaseUrl + `/deletefab/${encodeURIComponent(movieTitle)}`, {
+            method: "DELETE",
+            headers: {
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
+            }
+        })
+            .then((response) => {
+                if (response.status == 201) {
+                    window.location.reload();
+                } else {
+                    alert("Unable to remove movie from favorite list");
+                }
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
     return (
         <>
-            <div>
-                <h5>Favourites Movies</h5>
+            <h3 style={{ textAlign: "center" }}>Favourites Movies</h3>
+            <Row className="justify-content-md-center">
                 {
                     favMovies.map((movie) => (
-                        <Col className="mt-2" key={movie.id}>
+                        <Col className="mb-5" key={movie.id} md="auto">
                             <MovieCard movie={movie} />
+                            <Button onClick={(event) => removeFavHandler(movie.title, event)} variant="danger" size="xs">Remove</Button>
                         </Col>
                     ))}
-            </div>
+            </Row>
         </>
     )
-
 }
