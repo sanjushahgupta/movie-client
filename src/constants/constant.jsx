79c64ba35726acce0e7e3a582892f8f95010701b
Sanju Shah <sanjushahgupta@gmail.com>
2023-11-22 11:52:25 +0100

    save base url in env

--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -1,3 +1,5 @@
-export const BaseUrl = "https://flix-api-1faf.onrender.com";
-export const loggedInUser = JSON.parse(localStorage.getItem('user'));
+export const BaseUrl = process.env.BASE_URL;
+//"https://flix-api-1faf.onrender.com";
+
+export const loggedInUser = JSON.parse(localStorage.getItem("user"));
 export const token = localStorage.getItem("token");
